78d6e3d2832ec4925299ecaad340d55f2601dc72
Abdukhaligov <hikmat.pou@gmail.com>
2022-03-21 23:06:58 +0400

    add: Session (db_staff)

---
+++ database/migrations/2022_03_21_185420_create_sessions_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSessionsTable extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::connection('db_staff')->create('sessions', function (Blueprint $table) {
+      $table->id();
+
+      $table->bigInteger('instance_id')->unsigned()->nullable();
+
+      $table->bigInteger('staff_id')->unsigned()->nullable();
+      $table->foreign('staff_id')->on('employees')->references('id');
+
+      $table->integer('initial_time')->default(0);
+      $table->integer('updated_time')->default(0);
+
+      $table->double('initial_price')->default(0);
+      $table->double('updated_price')->default(0);
+
+      // MORNING, EVENING, EXTRA
+      $table->string('tariff');
+
+      $table->boolean('tariff_changed');
+
+      // 'QUEUE', 'ACTIVE', 'CANCELED', 'FINISHED'
+      $table->string('status')->default('queue');
+
+      $table->text('note')->nullable();
+
+      $table->dateTime('start_time')->default(null)->nullable();
+      $table->dateTime('end_time')->default(null)->nullable();
+
+      $table->bigInteger('written_by')->unsigned()->nullable();
+      $table->foreign('written_by')->on('employees')->references('id');
+
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::connection('db_staff')->dropIfExists('sessions');
+  }
+}
--- database/migrations/2022_03_21_190148_create_invoice_session_table.php
+++ database/migrations/2022_03_21_190148_create_invoice_session_table.php
@@ -4,19 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateInvoiceSnackSaleTable extends Migration {
+class CreateInvoiceSessionTable extends Migration {
   /**
    * Run the migrations.
    *
    * @return void
    */
   public function up() {
-    Schema::connection('db_staff')->create('invoice_snack_sale', function (Blueprint $table) {
+    Schema::connection('db_staff')->create('invoice_session', function (Blueprint $table) {
       $table->bigInteger('invoice_id')->unsigned();
-      $table->bigInteger('snack_sale_id')->unsigned();
+      $table->bigInteger('session_id')->unsigned();
 
       $table->foreign('invoice_id')->on('invoices')->references('id')->onDelete('CASCADE');
-      $table->foreign('snack_sale_id')->on('snack_sales')->references('id')->onDelete('CASCADE');
+      $table->foreign('session_id')->on('sessions')->references('id')->onDelete('CASCADE');
     });
   }
 
@@ -26,6 +26,6 @@ class CreateInvoiceSnackSaleTable extends Migration {
    * @return void
    */
   public function down() {
-    Schema::connection('db_staff')->dropIfExists('invoice_snack_sale');
+    Schema::connection('db_staff')->dropIfExists('invoice_session');
   }
 }

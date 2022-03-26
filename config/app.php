57ffd193ebf0e65c6f33240230470a5a42f091b5
Abdukhaligov <hikmat.pou@gmail.com>
2022-03-26 21:57:29 +0400

    change: auth server .env

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
   'name' => env('APP_NAME', 'Laravel'),
 
-  'auth_server' => env('AUTH_SERVER', 'http://phober-auth-server/'),
+  'auth_server' => env('AUTH_SERVER', 'http://localhost:8500/'),
 
   /*
   |--------------------------------------------------------------------------

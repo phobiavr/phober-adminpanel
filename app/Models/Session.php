eaddfa097ab122293eb22a37d59e9f746e0a0749
Abdukhaligov <hikmat.pou@gmail.com>
2024-08-30 05:51:04 +0400

    add: Session & update schedule

--- app/Models/Session.php
+++ app/Models/Session.php
@@ -14,18 +14,8 @@ class Session extends Model {
     protected $connection = "db_staff";
 
     protected $casts = [
-        "start_time" => "datetime",
-        "end_time"   => "datetime",
+        "time" => "datetime",
     ];
-
-    protected static function boot(): void {
-        parent::boot();
-
-        static::creating(function ($model) {
-            $model->updated_time = $model->initial_time;
-            $model->updated_price = $model->initial_price;
-        });
-    }
 
     public function instance(): BelongsTo {
         return $this->belongsTo(DeviceInstance::class, 'instance_id');

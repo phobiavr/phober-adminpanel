10b2e77538cb2b061b1b63b88c4b0f243ee7acd0
Abdukhaligov <hikmat.pou@gmail.com>
2024-09-19 02:52:00 +0400

    add: .env file for docker-compose.yml

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return true;
 });
+
+Route::get('/health', function () {
+    return response()->json(['status' => 'OK']);
+});

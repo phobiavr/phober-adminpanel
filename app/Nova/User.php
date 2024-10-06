4bf893a5cee8082528b8d410a045b081df9f449a
Abdukhaligov <hikmat.pou@gmail.com>
2024-10-06 04:16:07 +0400

    update: User class & remove unnecessary files

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -10,53 +10,43 @@ use Laravel\Nova\Fields\Password;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
-class User extends Resource
-{
-    /**
-     * The model the resource corresponds to.
-     *
-     * @var class-string<\App\Models\User>
-     */
-    public static $model = \App\Models\User::class;
-
-    /**
-     * The single value that should be used to represent the resource when being displayed.
-     *
-     * @var string
-     */
-    public static $title = 'name';
-
-    /**
-     * The columns that should be searched.
-     *
-     * @var array
-     */
-    public static $search = [
-        'id', 'name', 'email',
-    ];
+class User extends Resource {
+    public static string $model = \App\Models\User::class;
+    public static $title = 'username';
+    public static $search = ['first_name', 'last_name', 'username', 'email'];
+    public static $group = "Auth";
 
     /**
      * Get the fields displayed by the resource.
      *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
      * @return array
      */
-    public function fields(NovaRequest $request)
-    {
+    public function fields(NovaRequest|Request $request) {
         return [
             ID::make()->sortable(),
 
             Gravatar::make()->maxWidth(50),
 
-            Text::make('Name')
+            Text::make('Username')
+                ->sortable()
+                ->rules('required', 'max:255')
+                ->creationRules("unique:{$this->getTable()},username")
+                ->updateRules("unique:{$this->getTable()},username,{{resourceId}}"),
+
+            Text::make('First name')
+                ->sortable()
+                ->rules('required', 'max:255'),
+
+            Text::make('Last name')
                 ->sortable()
                 ->rules('required', 'max:255'),
 
             Text::make('Email')
                 ->sortable()
                 ->rules('required', 'email', 'max:254')
-                ->creationRules('unique:users,email')
-                ->updateRules('unique:users,email,{{resourceId}}'),
+                ->creationRules("unique:{$this->getTable()},email")
+                ->updateRules("unique:{$this->getTable()},email,{{resourceId}}"),
 
             Password::make('Password')
                 ->onlyOnForms()
@@ -68,44 +58,40 @@ class User extends Resource
     /**
      * Get the cards available for the request.
      *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
      * @return array
      */
-    public function cards(NovaRequest $request)
-    {
+    public function cards(NovaRequest $request) {
         return [];
     }
 
     /**
      * Get the filters available for the resource.
      *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
      * @return array
      */
-    public function filters(NovaRequest $request)
-    {
+    public function filters(NovaRequest $request) {
         return [];
     }
 
     /**
      * Get the lenses available for the resource.
      *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
      * @return array
      */
-    public function lenses(NovaRequest $request)
-    {
+    public function lenses(NovaRequest $request) {
         return [];
     }
 
     /**
      * Get the actions available for the resource.
      *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
      * @return array
      */
-    public function actions(NovaRequest $request)
-    {
+    public function actions(NovaRequest $request) {
         return [];
     }
 }

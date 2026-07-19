594272478f9586ef917779f60b60f40f6a56fa34
Hikmat Abdukhaligov <hikmat.abdukhaligov@americor.com>
2026-07-19 19:26:41 +0400

    remove: db-reset api

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/db-reset', function () {
-    Artisan::call('db:reset');
-    return Response::make(Artisan::output());
-});
